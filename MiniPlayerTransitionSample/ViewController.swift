6f698d654655dd9fdf6e291a521d2d7ca7684a71
Shoichi Kuraoka <shoichi.kuraoka.ts@mixi.co.jp>
2019-05-13 18:57:37 +0900

    Initial Commit

---
+++ MiniPlayerTransitionSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MiniPlayerTransitionSample
+//
+//  Created by 倉岡 昭一 on 2019/05/13.
+//  Copyright © 2019 ShoichiKuraoka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MiniPlayerTransitionSampleTests/MiniPlayerTransitionSampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  MiniPlayerTransitionSampleTests.swift
+//  MiniPlayerTransitionSampleTests
+//
+//  Created by 倉岡 昭一 on 2019/05/13.
+//  Copyright © 2019 ShoichiKuraoka. All rights reserved.
+//
+
+import XCTest
+@testable import MiniPlayerTransitionSample
+
+class MiniPlayerTransitionSampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
